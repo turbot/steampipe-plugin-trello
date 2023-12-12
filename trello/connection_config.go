c3b4f46bd4c6768b7e64d0599bd9907cd991f3a4
Ved misra <47312748+misraved@users.noreply.github.com>
2023-12-12 17:16:07 +0530

    Recompile plugin with steampipe-plugin-sdk v5.8.0 (#16)

--- trello/connection_config.go
+++ trello/connection_config.go
@@ -2,21 +2,11 @@ package trello
 
 import (
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/schema"
 )
 
 type trelloConfig struct {
-	APIKey *string `cty:"api_key"`
-	Token  *string `cty:"token"`
-}
-
-var ConfigSchema = map[string]*schema.Attribute{
-	"api_key": {
-		Type: schema.TypeString,
-	},
-	"token": {
-		Type: schema.TypeString,
-	},
+	APIKey *string `hcl:"api_key"`
+	Token  *string `hcl:"token"`
 }
 
 func ConfigInstance() interface{} {
